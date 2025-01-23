542d990470b03ed4b83205136dd5efab2b77eb9b
Maria Riojas <riojas310@gmail.com>
2025-01-23 16:42:40 -0500

    reemplazando archivos

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { AmazonConnectApp } from "@amazon-connect/app";
+import React from "react";
+import ReactDOM from "react-dom";
+import {AmazonConnectAgent} from "./components/AmazonConnectAgent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Holi</h1>
+
     </div>
   );
 }
